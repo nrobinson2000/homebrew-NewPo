cf82bb3ee887a91c26765eedb6a6e72413667409
Nathan Robinson <nrobinson2000@me.com>
2018-04-10 00:19:40 -0400

    Update NewPo.rb

--- NewPo.rb
+++ NewPo.rb
@@ -16,7 +16,7 @@ class Newpo < Formula
   desc "Particle Offline Utility: The Ultimate Local Particle Experience"
   homepage 'https://po-util.com'
   head 'https://github.com/nrobinson2000/homebrew-NewPo', :using => :git
-  url 'https://github.com/nrobinson2000/po/archive/0.2.1.tar.gz'
+  url 'https://github.com/nrobinson2000/po/archive/0.2.2.tar.gz'
   sha256 'e93a2515d418360a8f32845278dc323ae605d036b9e8c5655e4c48aed63440ae'
 
   def install
