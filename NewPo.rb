0534daa42b227fe4845a7a22d1069aad734c8cfc
Nathan Robinson <nrobinson2000@me.com>
2018-04-10 22:04:08 -0400

    Update NewPo.rb

--- NewPo.rb
+++ NewPo.rb
@@ -17,7 +17,7 @@ class Newpo < Formula
   homepage 'https://po-util.com'
   head 'https://github.com/nrobinson2000/homebrew-NewPo', :using => :git
   url 'https://github.com/nrobinson2000/po/archive/0.2.4.tar.gz'
-  sha256 '50386c6e461b608b7e76b2bd4b6fa33cf8ead0b6f4122795cfae63db82aa8a3e'
+  sha256 'c1179a770dc6029c46fe778a5285e5c00156e1cad81ce5b5827f15fac2fb1f91'
 
   def install
     bin.install 'bin/po'
